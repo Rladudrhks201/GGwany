8779efc20e582e779cd6fcf60fa6f874b27c450e
Rladudrhks201 <114721570+Rladudrhks201@users.noreply.github.com>
2023-01-26 18:23:30 +0900

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,25 +1,46 @@
 import torch
 
-BATCH_SIZE = 24  # GPU memory size
-RESIZE_TO = 512 # resize the image training and transforms
+BATCH_SIZE = 10  # GPU Memory size
+RESIZE_TO = 512  # resize the image training and transforms
 NUM_EPOCHS = 100 # number of epochs to train for
+NUM_WORKER = 2   # number of cpu workers
 
-DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # train image and xml files directory
-TRAIN_DIR = "../Microcontroller Detection/train"
-# validation image and xml files directory
-VALID_DIR = "../Microcontroller Detection/test"
+TRAIN_DIR = '..\\Microcontroller Detection\\train'
 
-CLASSES = [
-    'background', 'Arduino_Nano', 'ESP8266', 'Raspberry_Pi_3', 'Heltec_ESP32_Lora'
-]
+# Validation image and xml files directory
+VALID_DIR = '..\\Microcontroller Detection\\test'
+
+CLASSES = ['background', 'Arduino_Nano', 'ESP8266', 'Raspberry_Pi_3', 'Heltec_ESP32_Lora']
 NUM_CLASSES = 5
+# two stage detection이라 배경도 봄
+
+CARD_TRAIN_DIR = '..\\PokerCard Detection\\train'
+CARD_VALID_DIR = '..\\PokerCard Detection\\valid'
+CARD_TEST_DIR = '..\\PokerCard Detection\\test'
+
+CARD_CLASSES = ['background','10 Diamonds', '10 Hearts', '10 Spades', '10 Trefoils','2 Diamonds', '2 Hearts', '2 Spades',
+                '2 Trefoils','3 Diamonds', '3 Hearts', '3 Spades', '3 Trefoils',
+                '4 Diamonds', '4 Hearts', '4 Spades', '4 Trefoils','5 Diamonds',
+                '5 Hearts', '5 Spades', '5 Trefoils', '59','6 Diamonds', '6 Hearts', '6 Spades', '6 Trefoils',
+                '7 Diamonds', '7 Hearts', '7 Spades', '7 Trefoils',
+                '8 Diamonds', '8 Hearts', '8 Spades', '8 Trefoils',
+                '9 Diamonds', '9 Hearts', '9 Spades', '9 Trefoils',
+                'A Diamonds', 'A Hearts', 'A Spades', 'A Trefoils',
+                'J Diamonds', 'J Hearts', 'J Spades', 'J Trefoils',
+                'K Diamonds', 'K Hearts', 'K Spades', 'K Trefoils',
+                'Q Diamonds', 'Q Hearts', 'Q Spades', 'Q Trefoils']
+CARD_NUM_CLASSES = 53
 
 # 데이터 로더 생성 후 이미지 시각화 여부
 VISUALIZE_TRANSFORMED_IMAGES = False
 
 # location to save model and plots
-OUT_DIR = "../outputs"
-SAVE_PLOTS_EPOCH = 2 # save loss plots after these many epochs
-SAVE_MODEL_EPOCH = 2 # save model after these many epochs
+OUT_DIR = '..\\outputs'
+SAVE_PLOTS_EPOCH = 2
+SAVE_MODEL_EPOCH = 2
+# save loss plots, models after these many epochs
+
+NUM_SAMPLES_TO_VISUALIZE = 5
--- src/datasets.py
+++ src/datasets.py
@@ -9,6 +9,7 @@ from config import *
 from torch.utils.data import Dataset, DataLoader
 from utils import collate_fn, get_train_transform, get_valid_transform
 
+
 # the dataset class
 class MicrocontrollerDataset(Dataset):
     def __init__(self, dir_path, width, height, classes, transform=None):
@@ -17,55 +18,124 @@ class MicrocontrollerDataset(Dataset):
         self.height = height
         self.classes = classes
         self.transform = transform
-        
+
         # get all the image paths in sorted order
-        self.image_paths = glob.glob(f'{self.dir_path}/*.jpg')
-        self.all_images = [image_path.split('/')[-1] for image_path in self.image_paths]
+        self.image_paths = glob.glob(f'{self.dir_path}\\*.jpg')
+        self.all_images = [image_path.split('\\')[-1] for image_path in self.image_paths]
         self.all_images = sorted(self.all_images)
-        
+
     def __getitem__(self, index):
         # capture the image name and the full image path
         image_name = self.all_images[index]
         image_path = os.path.join(self.dir_path, image_name)
-        
+
         # read the image
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
         image_resized = cv2.resize(image, (self.width, self.height))
         image_resized /= 255.0
-        
+
         # capture the corresponding xml file for getting the annotations
         ann_filename = image_name[:-4] + '.xml'
         ann_filepath = os.path.join(self.dir_path, ann_filename)
-        
+
         boxes = []
         labels = []
         tree = ET.parse(ann_filepath)
         root = tree.getroot()
-        
+
         # get the height, width of the image
         image_width = image.shape[1]
         image_height = image.shape[0]
-        
-        for member in root.findall('object')
+
+        for member in root.findall('object'):
             labels.append(self.classes.index(member.find('name').text))
-            
+
             # xmin
-            xmin = int(member.find('bondbox').find('xmin').text)
+            xmin = int(member.find('bndbox').find('xmin').text)
             # xmax
-            xmax = int(member.find('bondbox').find('xmax').text)
+            xmax = int(member.find('bndbox').find('xmax').text)
             # ymin
-            ymin = int(member.find('bondbox').find('ymin').text)
+            ymin = int(member.find('bndbox').find('ymin').text)
             # ymax
-            ymax = int(member.find('bondbox').find('ymax').text)
-            
+            ymax = int(member.find('bndbox').find('ymax').text)
+
             # resize the bounding boxes according to the adjusted image size
             xmin_final = (xmin / image_width) * self.width
             xmax_final = (xmax / image_width) * self.width
             ymin_final = (ymin / image_height) * self.height
             ymax_final = (ymax / image_height) * self.height
-            
+
             boxes.append([xmin_final, ymin_final, xmax_final, ymax_final])
-            
+
+        # bbox to tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # area of bounding boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        # labels to tensor
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        target['area'] = area
+        target['iscrowd'] = iscrowd
+        image_id = torch.Tensor([index])
+        target['image_id'] = image_id
+
+        # apply the image transforms
+        if self.transform:
+            sample = self.transform(image=image_resized, bboxes=target['boxes'],
+                                    labels=labels)
+            image_resized = sample['image']
+            target['boxes'] = torch.Tensor(sample['bboxes'])
+
+        return image_resized, target
+
     def __len__(self):
-        pass
+        return len(self.all_images)
+
+
+# prepare the final datasets and data loaders
+train_dataset = MicrocontrollerDataset(TRAIN_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_train_transform())
+valid_dataset = MicrocontrollerDataset(VALID_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_valid_transform())
+
+# card dataset
+card_train_dataset = MicrocontrollerDataset(CARD_TRAIN_DIR, RESIZE_TO, RESIZE_TO, CARD_CLASSES, get_train_transform())
+card_valid_dataset = MicrocontrollerDataset(CARD_VALID_DIR, RESIZE_TO, RESIZE_TO, CARD_CLASSES, get_valid_transform())
+card_test_dataset = MicrocontrollerDataset(CARD_TEST_DIR, RESIZE_TO, RESIZE_TO, CARD_CLASSES, get_valid_transform())
+
+# dataloader
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER,
+                          collate_fn=collate_fn)
+valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKER,
+                          collate_fn=collate_fn)
+
+# card data loader
+card_train_loader = DataLoader(card_train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER,
+                          collate_fn=collate_fn)
+card_valid_loader = DataLoader(card_valid_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKER,
+                          collate_fn=collate_fn)
+# if __name__ == '__main__':
+#     dataset = MicrocontrollerDataset(CARD_TRAIN_DIR, RESIZE_TO, RESIZE_TO, CARD_CLASSES)
+#     print(f'Number of Train Images >> {len(card_train_dataset)}')
+#
+#
+#     # function to visualize a single sample
+#     def visualize_sample(image, target):
+#         box = target['boxes'][0]
+#         label = CLASSES[target['labels']]
+#
+#         cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
+#
+#         cv2.putText(
+#             image, label, (int(box[0]), int(box[1] - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
+#         )
+#
+#         cv2.imshow('show', image)
+#         cv2.waitKey(0)
+#
+#     for i in range(NUM_SAMPLES_TO_VISUALIZE):
+#         image, target = dataset[i]
+#         visualize_sample(image, target)
